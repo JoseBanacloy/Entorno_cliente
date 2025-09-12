b8613af50bed206faf56b1f2200cfb3d5339f02f
JoseBanacloy <josbancar@alu.edu.gva.es>
2025-09-12 17:35:05 +0000

    ya porfin funciona

--- primeraPruebas/primeraEj.js
+++ primeraPruebas/primeraEj.js
@@ -1,3 +1,15 @@
-let numHaAdivinar = prompt("Dime un numero del 1 al 50 para adiviniarlo");
+var numHaAdivinar = prompt("Dime un numero del 1 al 50 para adiviniarlo");
 console.log(numHaAdivinar);
-let buscadorNum = Math.floor(Math.random(1, 50));
+
+let buscadorNum = parseInt(Math.random()*50 + 1);
+
+console.log(buscadorNum);
+if (parseInt(buscadorNum) < parseInt(numHaAdivinar)){
+    console.log("Te pasaste jefe!");
+}else if(parseInt(buscadorNum) > parseInt(numHaAdivinar)){
+    console.log("Te quedaste corto!");
+}
+else{
+    console.log("Has dado en el clavo master!");
+    alert("Has dado en el clavo master");
+}
